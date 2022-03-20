30fd4fe5c4aa7326a05e75f920804d612617922a
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-03-21 00:25:48 +0800

    Added total earnings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,14 +29,27 @@ interface Data {
 const Dashboard = (props: SalesProps) => {
   return (
     <div className="flex flex-col mx-auto w-3/4 h-screen p-4">
-      <Inventory />
+      <Earning orders={props.orders} />
       <DailySales orders={props.orders} />
+      {/* Top 5 Tables */}
     </div>
   );
 };
 
-const Inventory = () => {
-  return <div></div>;
+const Earning = (props: SalesProps) => {
+  return (
+    <div className="bg-gradient-to-r from-yellow-light to-[#fffced] max-w-md p-4 rounded-md mb-4">
+      <h1 className="text-2xl font-semibold">
+        &#x20B1;
+        {props.orders.reduce(
+          (acc, order) => acc + order.product.price * order.numberOfItems,
+          0
+        )}
+      </h1>
+      <div className="m-2"></div>
+      <h2 className="text-lg font-semibold">Total Earnings</h2>
+    </div>
+  );
 };
 
 const DailySales = (props: SalesProps) => {
