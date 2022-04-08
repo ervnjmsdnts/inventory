eeac1d90b485bd61c1a988ee4d85b8ea9dc36614
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-04-08 16:32:34 +0800

    made category page responsive

--- src/components/category/CategoryHeader.tsx
+++ src/components/category/CategoryHeader.tsx
@@ -7,11 +7,13 @@ const CategoryHeader = () => {
 
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-4">Category</h1>
+      <h1 className="text-4xl font-bold mt-4 md:mt-0 mb-4 text-center md:text-left">
+        Category
+      </h1>
       <button
         type="button"
         onClick={() => setIsOpen(true)}
-        className="bg-yellow-light mb-2 px-4 py-2 flex items-center rounded-md">
+        className="bg-yellow-light mb-2 px-4 py-2 flex items-center mx-auto md:mx-0 rounded-md">
         <div className="pr-4">
           <BiPlus size={24} />
         </div>
