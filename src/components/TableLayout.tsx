5ec1952b31f081170ba0c77249e56a89ff4fe64d
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-04-08 15:39:02 +0800

    test

--- src/components/TableLayout.tsx
+++ src/components/TableLayout.tsx
@@ -5,15 +5,9 @@ interface ITable extends React.HTMLAttributes<HTMLDivElement> {
 const TableLayout = ({ children, ...props }: ITable) => {
   return (
     <div className={`flex flex-col ${props}`}>
-      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="drop-shadow-md overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="w-full divide-y divide-gray-200">
-              {children}
-            </table>
-          </div>
-        </div>
-      </div>
+      <table className="w-2/4 divide-y divide-gray-200 drop-shadow">
+        {children}
+      </table>
     </div>
   );
 };
