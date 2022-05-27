4334964794c78bd704ad62492a510eb771f8ab8c
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-05-27 21:06:14 +0800

    more revisions

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form";
 import { UserConsumer } from "../context/authContext";
 import { useEffect, useState } from "react";
 import Slider from "../components/Slider";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
 const Login = () => {
   const { register, handleSubmit } = useForm();
@@ -42,59 +43,70 @@ const Login = () => {
   };
 
   return (
-    <>
-      <div className="flex flex-col md:flex-row items-center justify-evenly md:h-screen py-16">
-        <div className="flex justify-center flex-col items-center mb-16 md:m-0">
-          <h1 className="text-5xl text-gray-800 font-bold">Welcome Back!</h1>
-          <p className="text-xl text-gray-500">Please enter your details</p>
-          <div className="relative h-32 w-32 mt-8">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              layout="fill"
-              className="rounded-full"
-              priority={true}
-            />
-          </div>
-          <Slider />
+    <div className="flex flex-col md:flex-row items-center justify-evenly md:h-screen py-16">
+      <div className="flex justify-center flex-col items-center mb-16 md:m-0">
+        <h1 className="text-5xl text-gray-800 font-bold">Cha Bear-Balete</h1>
+        <div className="relative h-32 w-32 mt-8">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            layout="fill"
+            className="rounded-full"
+            priority={true}
+          />
         </div>
-        <div className="bg-yellow-light w-full md:w-2/5 md:rounded-3xl">
-          <form
-            onSubmit={handleSubmit(onSubmitForm)}
-            className="flex flex-col px-24 md:px-48 py-64 gap-4">
-            <h2 className="text-4xl font-bold text-black mb-4">Login</h2>
-            <div className="flex flex-col gap-2">
-              <p className="text-xl font-bold">Username</p>
-              <input
-                type="text"
-                className="w-full bg-gray-100 text-gray-900 rounded-md pl-2 h-12"
-                placeholder="Enter your username..."
-                {...register("username", { required: true })}
-              />
+        <Slider />
+        <div className="flex pt-8 flex-col gap-4 self-start">
+          <div className="flex items-center">
+            <div className="text-4xl pr-4">
+              <FaFacebook />
             </div>
-            <div className="flex flex-col gap-2">
-              <p className="text-xl font-bold">Password</p>
-              <input
-                type="password"
-                className="w-full bg-gray-100 text-gray-900 rounded-md pl-2 h-12"
-                placeholder="Enter your password..."
-                {...register("password", { required: true })}
-              />
+            <h5 className="text-xl">ChaBear - Balete, Batangas</h5>
+          </div>
+          <div className="flex items-center">
+            <div className="text-4xl pr-4">
+              <FaInstagram />
             </div>
-            <div className="flex justify-center">
-              <button
-                type="submit"
-                className="bg-yellow-dark text-brown-dark font-semibold rounded-md text-lg mt-4 px-16 py-2">
-                Login
-              </button>
-            </div>
-            <div className="flex justify-center items-center">
-              <span className="text-red-500 mt-4 text-lg">{error}</span>
-            </div>
-          </form>
+            <h5 className="text-xl">chabear.balete</h5>
+          </div>
         </div>
       </div>
-    </>
+      <div className="bg-yellow-light w-full md:w-2/5 md:rounded-3xl">
+        <form
+          onSubmit={handleSubmit(onSubmitForm)}
+          className="flex flex-col px-24 md:px-48 py-64 gap-4">
+          <h2 className="text-4xl font-bold text-black mb-4">Login</h2>
+          <div className="flex flex-col gap-2">
+            <p className="text-xl font-bold">Username</p>
+            <input
+              type="text"
+              className="w-full bg-gray-100 text-gray-900 rounded-md pl-2 h-12"
+              placeholder="Enter your username..."
+              {...register("username", { required: true })}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-xl font-bold">Password</p>
+            <input
+              type="password"
+              className="w-full bg-gray-100 text-gray-900 rounded-md pl-2 h-12"
+              placeholder="Enter your password..."
+              {...register("password", { required: true })}
+            />
+          </div>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-yellow-dark text-brown-dark font-semibold rounded-md text-lg mt-4 px-16 py-2">
+              Login
+            </button>
+          </div>
+          <div className="flex justify-center items-center">
+            <span className="text-red-500 mt-4 text-lg">{error}</span>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
